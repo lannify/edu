12619e09d634f5a9693236992b4b9088957c67db
Sourabh Pal <sourabhp203@gmail.com>
2015-10-25 05:09:01 -0700

    Extracted price, image, and item name from Target API

--- app/assets/javascripts/supplies/supplies.js
+++ app/assets/javascripts/supplies/supplies.js
@@ -9,14 +9,19 @@ angular.module('edu')
 
 		o.itemLookup = function(item_id) {
 			console.log(item_id);
-			var target_url = "https://api.target.com/items/v3/" + item_id.item_id + "?id_type=dpci&key=1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb";
+			var target_url = "https://api.target.com/items/v3/" + item_id.item_id + "?id_type=dpci&store_id=530&fields=all_fields_group&key=1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb";
 			console.log(target_url);
 			var item_name = [];
+			var item_price = [];
+			var item_image = [];
 			return $http.get(target_url).success(function(res){
 				console.log(res);
 				item_name.push(res.product_composite_response.items[0].general_description);
+				item_price.push(res.product_composite_response.items[0].store_product[0].price.display_original_price);
+				item_image.push(res.product_composite_response.items[0].image.internal_primary_image_url);
 				console.log(item_name);
-				o.supplies.push(item_name);
+				console.log(item_price);
+				o.supplies.push(item_name, item_price, item_image);
 				// angular.copy(res, o.supplies);
 			});
 		};
