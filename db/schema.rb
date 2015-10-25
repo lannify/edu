ca682fca1395374a1b725ee9ae379d592d3f2fd0
lannng <lannguyen309@gmail.com>
2015-10-24 18:24:20 -0700

    added name column on User table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024221543) do
+ActiveRecord::Schema.define(version: 20151025012152) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "title"
@@ -166,6 +166,7 @@ ActiveRecord::Schema.define(version: 20151024221543) do
     t.integer  "user_level"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
