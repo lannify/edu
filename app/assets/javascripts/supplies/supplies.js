423375a76241cab2dfe9327c95c781ba28ad306b
Sourabh Pal <sourabhp203@gmail.com>
2015-10-25 04:36:47 -0700

    Display item names in a list, front-end only

--- app/assets/javascripts/supplies/supplies.js
+++ app/assets/javascripts/supplies/supplies.js
@@ -11,9 +11,10 @@ angular.module('edu')
 			console.log(item_id);
 			var target_url = "https://api.target.com/items/v3/" + item_id.item_id + "?id_type=dpci&key=1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb";
 			console.log(target_url);
+			var item_name = [];
 			return $http.get(target_url).success(function(res){
 				console.log(res);
-				var item_name = res.product_composite_response.items[0].general_description;
+				item_name.push(res.product_composite_response.items[0].general_description);
 				console.log(item_name);
 				o.supplies.push(item_name);
 				// angular.copy(res, o.supplies);
