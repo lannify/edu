4e225a17ce2340eadcf63c8698308518cc9638c3
lannng <lannguyen309@gmail.com>
2015-10-24 17:02:58 -0700

    added navbar

--- app/assets/javascripts/courses/coursesCtrl.js
+++
@@ -1,1 +0,0 @@
-coursesCtrl.js
--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -1,1 +1,22 @@
-navCtrl.js
+angular.module('edu')
+.controller('NavCtrl', [
+	'$scope',
+	'Auth',
+	function($scope, Auth){
+		$scope.signedIn = Auth.isAuthenticated;
+		$scope.logout = Auth.logout;
+		Auth.currentUser().then(function (user){
+			$scope.user = user;
+		});
+		$scope.$on('devise:new-registration', function (e, user){
+			$scope.user = user;
+		});
+
+		$scope.$on('devise:login', function (e, user){
+			$scope.user = user;
+		});
+
+		$scope.$on('devise:logout', function (e, user){
+			$scope.user = {};
+		});
+	}]);
