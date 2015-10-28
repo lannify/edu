26b8c6aef889af11aabcaae03889eae4d12d5566
Sourabh Pal <sourabhp203@gmail.com>
2015-10-28 14:57:56 -0700

    Added ASIN lookup and config for Amazon Product API

---
+++ config/initializers/asin.rb
@@ -0,0 +1,5 @@
+ASIN::Configuration.configure do |config|
+	config.secret        = 'yybNU3LafRRNdc+f5KUJpDaTBCqbByIi77gZaENz'
+	config.key           = 'AKIAJFKN6QLSOX6IB3QA'
+	config.associate_tag = 'curriculearn'
+end
