b3480a27c069e3c45b3bdb3f0b54f462d086acf4
ilee <axionike@gmail.com>
2015-10-29 15:29:57 -0700

    Added instructorFactory to handle user requests

---
+++ app/assets/javascripts/dashboard/instructorFactory.js
@@ -0,0 +1,16 @@
+angular.module('edu')
+
+.factory('instructorFactory', ['$http',
+	function($http){
+		var instructors = {};
+
+		instructors.getAll = function() {
+
+			return $http.get('/users.json').success(function(data){
+				angular.copy(data, instructors);
+			});
+		};
+
+		return instructor;
+	}
+]);
