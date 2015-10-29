582dd9ff2a208a571ff8deff1ee98a73a9dc6e13
lannng <lannguyen309@gmail.com>
2015-10-29 11:21:34 -0700

    rerouted users to respective dashboards after registration

--- app/assets/javascripts/auth/authCtrl.js
+++ app/assets/javascripts/auth/authCtrl.js
@@ -20,7 +20,15 @@ function($scope, $state, Auth){
 
   $scope.register = function() {
     Auth.register($scope.user).then(function(){
-      $state.go('curriculums');
+      // $state.go('curriculums');
+      console.log(Auth._currentUser.user_level);
+      if (Auth._currentUser.user_level == 2) {
+        $state.go('instructor_dash');
+      } else if (Auth._currentUser.user_level == 3) {
+        $state.go('student_dash');
+      } else {
+        $state.go('curriculums');
+      }
     });
   };
 }]);
